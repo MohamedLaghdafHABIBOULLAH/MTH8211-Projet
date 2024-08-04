aedb1238ddd5e476e8a94bbcc559674b1bfce5ff
AlexLapointe <alexandre.lapointe@polymtl.ca>
2024-08-04 13:34:35 -0400

    Correction de l'ordre des alpha, beta et de la ligne 45

--- src/CS.jl
+++ src/CS.jl
@@ -10,14 +10,15 @@ Choisir σ_U et σ_L tq. 0 < σ_L ≤ σ_U pour que σ_i(A) ∈ [σ_L, σ_U] ∀
 =#
 
 function CS(A, b, ϵ)
+    m,n = size(A)
     Σ = svd(A).S
 
     # Trouve la plus grande v.s. de A et une deuxième valeur inférieure à celle-ci:
     σ_U = Σ[1]   
-    σ_L = rand(Σ)
+    σ_L = Σ[min(m,n)]
 
     # Définition des constantes et vecteurs initiaux nécessaires pour l'algorithme de Chebyshev:
-    m,n = size(A)
+
     d = (σ_U^2 + σ_L^2)/2
     c = (σ_U^2 - σ_L^2)/2
 
@@ -33,21 +34,22 @@ function CS(A, b, ϵ)
     # Début de la boucle itérative de Chebyshev:
     for k in 0:iter
         if k == 0
-            β = 0
             α = 1/d
+            β = 0
 
         elseif k ==1
-            β = 1/2 * (c/d)^2
             α = d - (c^2) / 2*d
+            β = 1/2 * (c/d)^2
 
         else
+            α = 1/(d-α * (c^2)/4)
             β = (α * c/2)^2
-            α = 1/((d-α * c^2)/4)
         end
 
         v = β * v + A' * r
         x = x + α * v
         r = r - α * A * v
+
     end
 
     return x
